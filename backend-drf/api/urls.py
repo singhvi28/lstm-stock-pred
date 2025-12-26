3978421e81deef1b6a1878075fc7c7d92c41466e
singhvi28 <akkisinghvi28@gmail.com>
2025-12-26 18:53:22 +0530

    backend apis

---
+++ backend-drf/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from accounts import views as UserViews
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', UserViews.RegisterView.as_view()),
+    # Add these lines to enable the JWT views
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
